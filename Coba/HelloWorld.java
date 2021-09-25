c4400ad3d02cc50155ddfff46e8cee4abaed0128
wulanmaulidya <91161995+wulanmaulidya@users.noreply.github.com>
2021-09-25 22:47:22 +0700

    HelloWorld.java

---
+++ Coba/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+	    System.out.println ("Nama : Wulan");
+		System.out.println ("NIM  : 6706213041");
+		System.out.println ("Kelas: D3IF4503");
+	}
+}	
